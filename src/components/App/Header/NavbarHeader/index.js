4f9a101ce667ed07db47dbd14d03fda8b9bce38e
Pancho Rifa <panchorifavato@gmail.com>
2019-04-26 07:00:26 -0600

    Fixing sidebar minification

--- src/components/App/Header/NavbarHeader/index.js
+++ src/components/App/Header/NavbarHeader/index.js
@@ -7,11 +7,12 @@ class NavbarHeader extends Component {
   render() {
     return (
       <div className="navbar-header">
-        <NavLink to="/" className="navbar-brand">WEBXFORMS</NavLink>
         <button type="button" className="navbar-toggle" data-click="sidebar-toggled">
           <span className="icon-bar"></span>
           <span className="icon-bar"></span>
         </button>
+
+        <NavLink to="/" className="navbar-brand">WEBXFORMS</NavLink>
       </div>
     )
   }
--- src/components/App/Header/index.js
+++ src/components/App/Header/index.js
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 import Navbar from './Navbar/index';
 import NavbarHeader from './NavbarHeader/index';
 import NavbarSearch from './NavbarSearch/index';
-// import './Header.css';
+import {NavLink} from 'react-router-dom';
+import './Header.css';
 
 class Header extends Component {
 
   render() {
     return (
       <header id="header" className="app-header">
-      	<button type="button" className="navbar-toggle navbar-toggle-minify" data-click="sidebar-minify">
-      		<span className="icon-bar"></span>
-      		<span className="icon-bar"></span>
-      	</button>
-        <NavbarHeader></NavbarHeader>
+        <button type="button" className="navbar-toggle navbar-toggle-minify" data-click="sidebar-minify">
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <NavLink to="/" className="navbar-brand">WEBXFORMS</NavLink>
       </header>
     )
   }
@@ -23,3 +24,10 @@ export default Header;
 
 // <Navbar></Navbar>
 // <NavbarSearch></NavbarSearch>
+// <button type="button" className="navbar-toggle navbar-toggle-minify" data-click="sidebar-minify">
+//   <span className="icon-bar"></span>
+//   <span className="icon-bar"></span>
+// </button>
+        // <NavLink to="/" className="navbar-brand">WEBXFORMS</NavLink>
+
+        // <NavbarHeader></NavbarHeader>
